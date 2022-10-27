3d6eed78cfde3747bc837285301bcbc4a759c5c7
dariofa99 <darioj99@gmail.com>
2022-10-26 22:44:01 -0500

    Cambios

--- app/Http/Controllers/CasesController.php
+++ app/Http/Controllers/CasesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use \Facades\App\Facades\NewPush;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
-use Redis;
+use Illuminate\Support\Facades\Redis;
 use App\Models\CaseM; 
 use DB;
 use App\Models\User;
--- config/app.php
+++ config/app.php
@@ -235,6 +235,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'AuditLog'   => App\Facades\AuditLog::class,
+        'Redis' => Illuminate\Support\Facades\Redis::class,
     ],
 
 ];
