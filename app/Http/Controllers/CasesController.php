12862955f5c80eb4485d40d91e1bc098919dc0cc
Jonathan Venegas <jonathan.9129@gmail.com>
2024-09-10 21:29:55 -0500

    Fix error duplicated cases

--- app/Http/Controllers/CasesController.php
+++ app/Http/Controllers/CasesController.php
@@ -80,7 +80,8 @@ class CasesController extends Controller
             ->getData($request)
             ->where('cases.type_status_id','<>','15')
             ->select('cases.created_at','cases.case_number','cases.id','rt.name as status','rt.options as color','rt.id as status_id','rtc.name as type_case','rtr.name as branch_law')
-            ->orderBy('cases.case_number','desc')->paginate(15);
+            ->orderBy('cases.case_number','desc')
+            ->groupBy('cases.case_number')->paginate(15);
         if((!$request->data and !$request->type)){
            /*  $cases=CaseM::join('references_table as rt','rt.id','cases.type_status_id')
             ->join('references_table as rtc','rtc.id','cases.type_case_id')
